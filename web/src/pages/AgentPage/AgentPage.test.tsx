4fb402a147c0464810d9a101f80e25f0c4631d1e
Jace <638764+jacebenson@users.noreply.github.com>
2023-12-17 22:26:18 -0600

    removed extra agent page and cell

--- web/src/pages/AgentPage/AgentPage.test.tsx
+++ web/src/pages/AgentPage/AgentPage.test.tsx
@@ -5,7 +5,7 @@ import AgentPage from './AgentPage'
 //   Improve this test with help from the Redwood Testing Doc:
 //   https://redwoodjs.com/docs/testing#testing-pages-layouts
 
-describe('AgentPage', () => {
+describe('Agent2Page', () => {
   it('renders successfully', () => {
     expect(() => {
       render(<AgentPage />)
