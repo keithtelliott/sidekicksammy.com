06caa1ccb5522d9feaccac843ab71aba629cc93a
Jace <638764+jacebenson@users.noreply.github.com>
2023-12-22 01:08:21 -0600

    added some utily fx

---
+++ scripts/deleteAll.ts
@@ -0,0 +1,15 @@
+// To access your database
+// Append api/* to import from api and web/* to import from web
+import { db } from 'api/src/lib/db'
+
+
+export default async ({ args }) => {
+  // Your script here...
+  console.log(':: Executing script with args ::')
+  console.log(args)
+  // delete all bots
+  await db.bot.deleteMany()
+  // delete all users
+  await db.user.deleteMany()
+
+}
