6c2df1c78aad05cdea89781a9f189b78447b27f2
Jace <638764+jacebenson@users.noreply.github.com>
2024-02-08 20:59:23 -0600

    re-enabled hubspot integration (#83)

--- api/src/lib/hubspot.ts
+++ api/src/lib/hubspot.ts
@@ -1,14 +1,14 @@
-
 import fetch from 'cross-fetch'
-import { slugify } from './text';
-import { generateHexColor, inverseColor } from './color';
-import { db } from './db';
-export let getAllContact = async ({ after }: { after?: string }) => {
+
+import { generateHexColor, inverseColor } from './color'
+import { db } from './db'
+import { slugify } from './text'
+export const getAllContact = async ({ after }: { after?: string }) => {
   console.log(`getAllContact: after: ${after}`)
-  let contacts: any[] = [];
-  let limit = 100;
-  let archived = false;
-  let properties = [
+  let contacts: any[] = []
+  const limit = 100
+  const archived = false
+  const properties = [
     'sidekick_color_primary',
     'sidekick_color_secondary',
     'sidekick_color_text',
@@ -21,84 +21,108 @@ export let getAllContact = async ({ after }: { after?: string }) => {
     'sidekick_greeting',
     'email',
     'id',
-  ];
-  let propertiesString = properties.join('&properties=');
-  let afterString = '';
+  ]
+  const propertiesString = properties.join('&properties=')
+  let afterString = ''
   if (after !== undefined) afterString = `&after=${after}`
-  let url = [
+  const url = [
     'https://api.hubapi.com/crm/v3/objects/contacts?',
     `limit=${limit}`,
     afterString,
     `&archived=${archived}`,
-    `&properties=${propertiesString}`
-  ];
-  let joinedUrl = url.join('');
+    `&properties=${propertiesString}`,
+  ]
+  const joinedUrl = url.join('')
   const response = await fetch(joinedUrl, {
     method: 'GET',
     headers: {
-      'Authorization': `Bearer ${process.env.SIDEKICKSAMMY_HUBSPOT_API_KEY}`,
-      'Content-Type': 'application/json'
+      Authorization: `Bearer ${process.env.SIDEKICKSAMMY_HUBSPOT_API_KEY}`,
+      'Content-Type': 'application/json',
     },
-  });
-  const data = await response.json();
+  })
+  const data = await response.json()
   // push the results into the contacts array
-  let results = data.results;
-  contacts = contacts.concat(results);
+  const results = data.results
+  contacts = contacts.concat(results)
   console.log(`getAllContact: contacts.length: ${contacts.length}`)
-  return contacts;
+  return contacts
 }
 // alias getHubspotContacts to getAllContact
-export let getSocialProof = async () => {
-  let allContacts = await getAllContact({ after: undefined });
+export const getSocialProof = async () => {
+  let allContacts = await getAllContact({ after: undefined })
   allContacts = allContacts.filter((contact) => {
-    if (contact.properties.sidekick_title && contact.properties.sidekick_logo_url) {
-      return true;
+    if (
+      contact.properties.sidekick_title &&
+      contact.properties.sidekick_logo_url
+    ) {
+      return true
     }
-    return false;
+    return false
   })
   allContacts = allContacts.map((contact) => {
-    let slugifiedSidekickTitle = slugify(contact.properties.sidekick_title || '');
+    const slugifiedSidekickTitle = slugify(
+      contact.properties.sidekick_title || ''
+    )
     return {
       logo: contact.properties.sidekick_logo_url,
       title: contact.properties.sidekick_title,
-      link: `/${slugifiedSidekickTitle}`
+      link: `/${slugifiedSidekickTitle}`,
     }
   })
-  return allContacts;
+  return allContacts
 }
-export let getContactBySidekickTitle = async ({ title }: { title: string }) => {
-  let allContacts = await getAllContact({ after: undefined });
-  let contact = allContacts.filter((contact) => {
+export const getContactBySidekickTitle = async ({
+  title,
+}: {
+  title: string
+}) => {
+  const allContacts = await getAllContact({ after: undefined })
+  const contact = allContacts.filter((contact) => {
     // we need to slugify the title and the sidekick_title
-    let slugifiedSidekickTitle = slugify(contact.properties.sidekick_title || '');
+    const slugifiedSidekickTitle = slugify(
+      contact.properties.sidekick_title || ''
+    )
     if (title == slugifiedSidekickTitle) {
-      return true;
+      return true
     }
-    return false;
+    return false
   })
-  return contact[0];
+  return contact[0]
 }
-export let mapHubspotContactToContact = ({ contact }: { contact: any }) => {
-  let primaryColor = contact?.properties?.sidekick_color_primary || generateHexColor();
-  let secondaryColor = contact?.properties?.sidekick_color_secondary || generateHexColor();
-  let textColor = contact?.properties?.sidekick_color_text || generateHexColor();
+export const mapHubspotContactToContact = ({ contact }: { contact: any }) => {
+  const primaryColor =
+    contact?.properties?.sidekick_color_primary || generateHexColor()
+  const secondaryColor =
+    contact?.properties?.sidekick_color_secondary || generateHexColor()
+  const textColor =
+    contact?.properties?.sidekick_color_text || generateHexColor()
   // lets send down the color scheme
-  let colorScheme = {
-    primaryColorScheme: { light: primaryColor, dark: inverseColor(primaryColor) },
-    secondaryColorScheme: { light: secondaryColor, dark: inverseColor(secondaryColor) },
+  const colorScheme = {
+    primaryColorScheme: {
+      light: primaryColor,
+      dark: inverseColor(primaryColor),
+    },
+    secondaryColorScheme: {
+      light: secondaryColor,
+      dark: inverseColor(secondaryColor),
+    },
     textColorScheme: { light: textColor, dark: inverseColor(textColor) },
     logo: contact?.properties?.sidekick_logo_url || '',
   }
 
   // lets get the opposite of the primary color
-  console.log('sidekick_fixie_agent_id: ' + contact?.properties?.sidekick_fixie_agent_id)
-  console.log('sidekick_fixie_corpus_id: ' + contact?.properties?.sidekick_fixie_corpus_id)
+  console.log(
+    'sidekick_fixie_agent_id: ' + contact?.properties?.sidekick_fixie_agent_id
+  )
+  console.log(
+    'sidekick_fixie_corpus_id: ' + contact?.properties?.sidekick_fixie_corpus_id
+  )
   return {
     id: contact?.id,
     fixieCorpusId: contact?.properties.sidekick_fixie_corpus_id,
     fixieAgentId: contact?.properties.sidekick_fixie_agent_id,
     sidekickTitle: contact?.properties.sidekick_title,
     sidekickGreeting: contact?.properties.sidekick_greeting,
-    sidekickColorScheme: JSON.stringify(colorScheme)
+    sidekickColorScheme: JSON.stringify(colorScheme),
   }
 }
